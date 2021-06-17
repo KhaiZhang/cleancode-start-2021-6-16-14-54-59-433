fd30f214e5d830d2b0fb63b011b365cc8be7a556
ZHANGKH <Khai.zhang@oocl.com>
2021-06-17 21:50:04 +0800

    [Khai][clean code][primitive obsession: refactor DeliverManager class]

--- src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/DeliveryManager.java
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/DeliveryManager.java
@@ -1,12 +1,16 @@
 package com.tw.academy.basic.$6_primitive_obsession.practiceOne;
 
 public class DeliveryManager {
+    private final Address from;
+    private final Address to;
     String toAddress;
     String fromAddress;
 
-    public DeliveryManager(String fromAddress, String toAddress) {
-        this.toAddress = toAddress;
-        this.fromAddress = fromAddress;
+    public DeliveryManager(Address fromAddress, Address toAddress) {
+        this.to = toAddress;
+        this.toAddress = this.to.getFromAddress();
+        this.from = fromAddress;
+        this.fromAddress = this.from.getFromAddress();
     }
 
     public DeliverCenter allocate(){
