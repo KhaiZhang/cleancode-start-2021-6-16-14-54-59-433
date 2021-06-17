0ef604df0a534a88116d6544521ffc9c6c72ed1d
ZHANGKH <Khai.zhang@oocl.com>
2021-06-17 13:40:00 +0800

    [Khai][clean code][rename some meaningless variable name]

--- src/main/java/com/tw/academy/basic/$4_naming/practiceOne/MorseCodeDecoder.java
+++ src/main/java/com/tw/academy/basic/$4_naming/practiceOne/MorseCodeDecoder.java
@@ -1,16 +1,22 @@
 package com.tw.academy.basic.$4_naming.practiceOne;
 
 public class MorseCodeDecoder {
-    public static String decode(String input) {
-        String[] tempValue = input.split("\\s{2,}");
-        StringBuilder res = new StringBuilder();
-        for (String s : tempValue) {
-            String [] charArray = s.split(" ");
-            for (String c: charArray) {
-                res.append(MorseCode.get(c));
+
+    public static final String MORSE_CODE_WORD_SEPARATOR = "\\s{2,}";
+    public static final String MORSE_CODE_SEPARATOR = " ";
+    public static final String WORD_SPARATOR = " ";
+    public static final String NULL_STR = "null";
+
+    public static String decode(String morseCodeStr) {
+        String[] morseCodeWords = morseCodeStr.split(MORSE_CODE_WORD_SEPARATOR);
+        StringBuilder decodedMorseCode = new StringBuilder();
+        for (String morseCodeWord : morseCodeWords) {
+            String [] morseCodeChars = morseCodeWord.split(MORSE_CODE_SEPARATOR);
+            for (String morseCodeChar: morseCodeChars) {
+                decodedMorseCode.append(MorseCode.get(morseCodeChar));
             }
-            res.append(" ");
+            decodedMorseCode.append(WORD_SPARATOR);
         }
-        return res.toString().replaceAll("null", "").trim();
+        return decodedMorseCode.toString().replaceAll(NULL_STR, "").trim();
     }
 }
