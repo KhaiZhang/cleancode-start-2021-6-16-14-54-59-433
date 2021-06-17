495801e6a1ee6bea042d9ec23aa69a0601839404
ZHANGKH <Khai.zhang@oocl.com>
2021-06-17 16:31:22 +0800

    [Khai][clean code][data clumps(auto): refactor Parcel class]

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Parcel.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Parcel.java
@@ -5,10 +5,6 @@ public class Parcel {
     private String size;
     private Double weight;
 
-    private String receiverName;
-    private String receiverPhoneNumber;
-    private String receiverAddress;
-
     private String senderName;
     private String senderPhoneNumber;
     private String senderAddress;
@@ -18,9 +14,6 @@ public class Parcel {
         this.size = size;
         this.weight = weight;
         this.receiver = receiver;
-        this.receiverName = this.receiver.getReceiverName();
-        this.receiverPhoneNumber = this.receiver.getReceiverPhoneNumber();
-        this.receiverAddress = this.receiver.getReceiverAddress();
         this.senderName = senderName;
         this.senderPhoneNumber = senderPhoneNumber;
         this.senderAddress = senderAddress;
@@ -29,7 +22,7 @@ public class Parcel {
     public String confirmReceiver(){
         return String.format("Please confirm receiver information: receiver name is %s, " +
                         "receiver phone number is %s and receiver address is %s.\n",
-                receiverName, receiverPhoneNumber, receiverAddress);
+                this.receiver.getReceiverName(), this.receiver.getReceiverPhoneNumber(), this.receiver.getReceiverAddress());
     }
 
     public String confirmSender(){
