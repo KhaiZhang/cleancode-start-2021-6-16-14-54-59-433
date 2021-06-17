6f974d69b2a7933252f8e81af1ba4675c507e5df
ZHANGKH <Khai.zhang@oocl.com>
2021-06-17 11:45:08 +0800

    [Khai][clean code][delete magic number]

--- src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
+++ src/main/java/com/tw/academy/basic/$2_magic_number/FareCalculation.java
@@ -2,12 +2,18 @@ package com.tw.academy.basic.$2_magic_number;
 
 public class FareCalculation {
 
+    public static final int CUMULATIVE_EXPENSE_NUMBER_100 = 100;
+    public static final int CUMULATIVE_EXPENSE_NUMBER_150 = 150;
+    public static final int CUMULATIVE_EXPENSE_NUMBER_400 = 400;
+    public static final double RATE_PERCENTAGE_80 = 0.8;
+    public static final double RATE_PERCENTAGE_50 = 0.5;
+
     public double calculate(double originalPrice, double cumulativeExpensesThisMonth) {
-        if (cumulativeExpensesThisMonth >= 100 && cumulativeExpensesThisMonth < 150) {
-            return originalPrice * 0.8;
+        if (cumulativeExpensesThisMonth >= CUMULATIVE_EXPENSE_NUMBER_100 && cumulativeExpensesThisMonth < CUMULATIVE_EXPENSE_NUMBER_150) {
+            return originalPrice * RATE_PERCENTAGE_80;
         }
-        if (cumulativeExpensesThisMonth >= 150 && cumulativeExpensesThisMonth < 400) {
-            return originalPrice * 0.5;
+        if (cumulativeExpensesThisMonth >= CUMULATIVE_EXPENSE_NUMBER_150 && cumulativeExpensesThisMonth < CUMULATIVE_EXPENSE_NUMBER_400) {
+            return originalPrice * RATE_PERCENTAGE_50;
         }
         return originalPrice;
     }
